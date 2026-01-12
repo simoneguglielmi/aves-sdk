75628bdac82f393f0c3d0acf5c52bcbea641feb0
simone.guglielmi <simone.guglielmi@synesthesia.it>
2026-01-12 14:10:14 +0100

    cleanup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,24 +1,5 @@
 import { defineConfig } from 'tsup';
 
-/**
- *  "tsup": {
-    "entry": [
-      "src/index.ts"
-    ],
-    "dts": {
-      "resolve": true,
-      "tsconfig": "tsconfig.build.json"
-    },
-    "format": [
-      "cjs",
-      "esm"
-    ],
-    "sourcemap": true,
-    "clean": true,
-    "minify": true
-  }
- */
-
 export default defineConfig({
   entry: ['src/index.ts'],
   dts: {
