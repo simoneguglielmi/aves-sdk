1556ee19c729dd34a3a9da5fde568f0993bd9b6f
simone.guglielmi <simone.guglielmi@synesthesia.it>
2026-01-12 14:14:54 +0100

    chore: lint

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,7 @@ import { join } from 'node:path';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  dts: {
-    resolve: true,
-    entry: join(__dirname, 'tsconfig.build.json'),
-  },
+  dts: { resolve: true, entry: join(__dirname, 'tsconfig.build.json') },
   format: ['cjs', 'esm'],
   sourcemap: true,
   clean: true,
